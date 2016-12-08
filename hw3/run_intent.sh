304d6957e43dc504a3384d0d0365d6b4c3657b52
chanhou <iepiechan@gmail.com>
2016-12-08 14:07:16 +0800

    found format bug of loading model

--- hw3/run_intent.sh
+++ hw3/run_intent.sh
@@ -1,5 +1,5 @@
 !/bin/bash
 
 python preprocess.py $1
-python rnn-nlu/run_multi-task_rnn_intent.py --data_dir rnn-nlu/data/atis --train_dir rnn-nlu/model_tmp --max_sequence_length 50 --task joint --bidirectional_rnn True --output $2
+python rnn-nlu/run_multi-task_rnn_intent.py --data_dir rnn-nlu/data/atis --train_dir rnn-nlu/why --max_sequence_length 50 --task joint --bidirectional_rnn True --output $2
 
