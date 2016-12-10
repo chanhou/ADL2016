b2bfe4cd8c99bac8e1260e97f778eb79cb0e70c9
chanhou <iepiechan@gmail.com>
2016-12-10 16:12:23 +0800

    add python code to download file from google drive

--- hw4/run_generation.sh
+++ hw4/run_generation.sh
@@ -1,3 +1,4 @@
 
 #python generate.py --data_dir ./data --data_dir_train ./data/train --data_dir_test ./data/valid --train_dir ./model --en_vocab_size=40000 --fr_vocab_size=40000 --final ./data/test.txt --write ./model/output --size=300
+python gdown.py 0B_9c-5D_LAa_Y1Jic05ISFVTOTA ./model_generate/translate.ckpt-57200
 python generate.py --data_dir ./data_generate --data_dir_train ./data_generate/train --data_dir_test ./data_generate/valid --train_dir ./model_generate --en_vocab_size=40000 --fr_vocab_size=40000 --final $1 --write $2 --size=300 --decode=True
